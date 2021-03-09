68ad267ce89d00e64d0fff503422cabe6eed39ce
Shashank Shekhar <shekharshashank@outlook.com>
2021-03-10 00:57:52 +0530

    Question3

---
+++ LeetCode/LeetCode/Question3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question3
+    {
+        static void Main(string[] args)
+        {
+            string s = "pwwkew";
+            Console.WriteLine(LengthOfLongestSubstring(s));
+        }
+
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            int i = 0; int j = 0;
+            int len = 0;
+            HashSet<char> hashSet = new HashSet<char>();
+            while(j < s.Length)
+            {
+                if(hashSet.Contains(s[j]) == false)
+                {
+                    hashSet.Add(s[j]);
+                    j++;
+                    len = Math.Max(len, j - i);
+                }
+                else
+                {
+                    hashSet.Remove(s[i]);
+                    i++;
+                }
+            }
+            return len;
+        }
+    }
+}
