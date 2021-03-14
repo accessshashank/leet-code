e56b191f3aa852de01cb84b8c3d6148dd8a0968e
Shashank Shekhar <shekharshashank@outlook.com>
2021-03-15 00:56:25 +0530

    Question7-8

---
+++ LeetCode/LeetCode/Question7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question7
+    {
+        static void Main(string[] args)
+        {
+            int x = 120;
+            Console.WriteLine(Reverse(x));
+        }
+
+        public static int Reverse(int x)
+        {
+            string str = (x < 0) ? (x*-1).ToString() : x.ToString();
+            string rev = "";
+            var array = str.ToCharArray();
+            Array.Reverse(array);
+            foreach(var ch in array)
+            {
+                rev += ch;
+            }
+
+            try
+            {
+                int val = int.Parse(rev);
+                var value = x < 0 ? val*-1 :  val;
+                return value;
+            }
+            catch(Exception ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Question8.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question8
+    {
+        static void Main(string[] args)
+        {
+            string s = "words and 987";
+            Console.WriteLine(MyAtoi(s));
+        }
+
+        public static int MyAtoi(string str)
+        {
+            string result = "";
+            var sign = 1;
+            str = str.TrimStart().TrimEnd();
+            if (string.IsNullOrEmpty(str)) return 0;
+            int index = 0;
+            if (str[0] == '+' || str[0] == '-')
+            {
+                sign = str[0] == '+' ? 1 : -1;
+                index++;
+            }
+            while (index < str.Length)
+            {
+                if (char.IsNumber(str[index]))
+                {
+                    result += str[index];
+                }
+                else
+                {
+                    break;
+                }
+                index++;
+            }
+
+            try
+            {
+                if (result == "") return 0;
+
+                var val = int.Parse(result);
+                return sign * val;
+            }
+            catch(Exception ex)
+            {
+                if (sign == 1)
+                    return int.MaxValue;
+                else
+                    return int.MinValue;
+            }
+        }
+    }
+}
