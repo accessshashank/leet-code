ed3088c0da5d78de9d1ea71d9daa5b0555d291e0
Shashank Shekhar <shekharshashank@outlook.com>
2021-03-17 00:37:16 +0530

    more-soln

---
+++ LeetCode/LeetCode/Question10.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question10
+    {
+        static void Main(string[] args)
+        {
+            string s = "aaa"; string p = "ab*a*c*a";
+            Console.WriteLine(IsMatch(s, p));
+        }
+
+        public static bool IsMatch(string s, string p)
+        {
+            int patternCounter = p.Length - 1;
+            int stringCounter = s.Length - 1;
+            bool match = false;
+            while(patternCounter >=0)
+            {
+                char ch = p[patternCounter];
+                if(ch == '.')
+                {
+                    patternCounter--;
+                    stringCounter--;
+                }
+                else if(ch == '*')
+                {
+                    patternCounter--;
+                    char previous = p[patternCounter];
+                    if(previous == '.')
+                    {
+                        match = true;
+                        break;
+                    }
+                    else
+                    {
+                        while(stringCounter >= 0 && s[stringCounter] == previous)
+                        {
+                            stringCounter--;
+                        }
+                        patternCounter--;
+                    }
+                }
+                else if(stringCounter >= 0 && p[patternCounter] == s[stringCounter])
+                {
+                    patternCounter--;
+                    stringCounter--;
+                }
+                else
+                {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (patternCounter ==  stringCounter)
+                match = true;
+
+            return match;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Question647.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question647
+    {
+        static void Main(string[] args)
+        {
+            string s = "abc";
+            Console.WriteLine(CountSubstrings(s));
+        }
+
+        public static int CountSubstrings(string s)
+        {
+            int length = s.Length;
+            int j; int i;
+            int counter = 0;
+            bool[,] dp = new bool[length, length];
+            for(int gap=0; gap<length; gap++)
+            {
+                j = gap;
+                i = 0;
+                while(j < length)
+                {
+                    if(gap == 0)
+                    {
+                        dp[i, j] = true;
+                    } 
+                    else if(gap == 1)
+                    {
+                        dp[i, j] = s[i] == s[j];
+                    }
+                    else
+                    {
+                        dp[i, j] = s[i] == s[j] && dp[i + 1, j - 1] == true;
+                    }
+
+                    if (dp[i, j] == true) counter++;
+
+                    i++;
+                    j++;
+
+                    
+                }
+
+
+            }
+
+            return counter;
+        }
+    }
+}
