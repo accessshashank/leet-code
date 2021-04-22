11a5b60d98cc83d876266cdca99a2c3954f8badb
Shashank Shekhar <shekharshashank@outlook.com>
2021-04-22 23:17:56 +0530

    Question11

---
+++ LeetCode/LeetCode/Question11.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question11
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 1, 2, 1 };
+            Console.WriteLine(MaxArea(input));
+        }
+
+        public static int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0; int right = height.Length - 1;
+            while(left < right)
+            {
+                int min = Math.Min(height[left], height[right]);
+                int area = min * (right - left);
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+
+                if(height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
