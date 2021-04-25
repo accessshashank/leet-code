24a89355c07f533155da611f13bc2f597a7b9f27
Shashank Shekhar <shekharshashank@outlook.com>
2021-04-25 17:55:01 +0530

    Question14

---
+++ LeetCode/LeetCode/Question14.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question14
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = new string[] { "flowrt", "flower", "flow", "flowht" };
+            Console.WriteLine(LongestCommonPrefix(strs));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+            string prefix = strs[0];
+            foreach (string str in strs)
+            {
+                if(str.Length < prefix.Length)
+                {
+                    prefix = str;
+                }
+            }
+
+            foreach (var str in strs)
+            {
+                while(str.Substring(0, prefix.Length) != prefix)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
