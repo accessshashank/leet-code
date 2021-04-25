923cbd9c53e5bb289cfd4c9eda653a5cab981915
Shashank Shekhar <shekharshashank@outlook.com>
2021-04-26 00:30:19 +0530

    Question20

---
+++ LeetCode/LeetCode/Question20.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question20
+    {
+        static void Main(string[] args)
+        {
+            string s = "[";
+            Console.WriteLine(IsValid(s));
+        }
+
+        public static bool IsValid(string s)
+        {
+            bool isValid = true;
+            var stack = new Stack<char>();
+            int counter = 0;
+            while(counter <= s.Length - 1)
+            {
+                char ch = s[counter];
+                if(ch == '[' || ch == '{' || ch == '(')
+                {
+                    stack.Push(ch);
+                }
+                if(ch == ']' || ch == '}' || ch == ')')
+                {
+                    if(stack.Count == 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    char pop = stack.Pop();
+                    if(ch == ']' && pop != '[')
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    else if (ch == '}' && pop != '{')
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    else if (ch == ')' && pop != '(')
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                counter++;
+            }
+
+            if (isValid == false) return false;
+
+            if (stack.Count == 0)
+            {
+                isValid = true;
+            }
+            else
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
