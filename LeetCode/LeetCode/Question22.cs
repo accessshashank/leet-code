61f21a04506fa2d044104813419ce2fe3005ce0d
Shashank Shekhar <shekharshashank@outlook.com>
2021-05-04 00:03:23 +0530

    Question22

---
+++ LeetCode/LeetCode/Question22.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Question22
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            var strings = GenerateParenthesis(n);
+        }
+
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            List<string> strings = new List<string>();
+            Generate("(", 1, 0, strings, n);
+            return strings;
+        }
+
+        private static void Generate(string current, int open, int closed, List<string> strings, int n)
+        {
+            if(current.Length == 2*n)
+            {
+                strings.Add(current);
+                return;
+            }
+            if (open < n) Generate(current + "(", open + 1, closed, strings, n);
+            if (closed < open) Generate(current + ")", open, closed + 1, strings, n);
+        }
+    }
+}
